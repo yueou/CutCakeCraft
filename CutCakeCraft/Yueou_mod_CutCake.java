6e9d16ddf6c35bfdcd133a0a935ec8a23c5f6726
XUQIMING <ahbbxqm@qq.com>
2012-12-04 19:08:20 +0800

    随便构建

---
+++ CutCakeCraft/Yueou_mod_CutCake.java
@@ -0,0 +1,46 @@
+package net.minecraft.src;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod( modid = "233", name="CutCake Craft", version="v0.1 by yueou")
+@NetworkMod
+(
+clientSideRequired = true,
+serverSideRequired = false,
+channels = {"cutcake"}
+//connectionHandler = ConnectionHandler.class,//TODO
+)
+
+public class Yueou_mod_CutCake {
+	
+	//@SidedProxy(clientSide = "net.minecraft.src.Yueou_CCClientProxy", serverSide = "net.minecraft.src.Yueou_CCCommonProxy")
+	//public static Yueou_CCCommonProxy proxy; //This object will be populated with the class that you choose for the environment
+	@Instance
+	public static Yueou_mod_CutCake instance; //The instance of the mod that will be defined, populated, and callable
+
+	public static Block cutcake;
+	@Init
+	public void load(FMLInitializationEvent evt)
+	{
+		cutcake = (new Yueou_BlockCutcake(1002,121)).setHardness(0.9F).setStepSound(Block.soundWoodFootstep).setBlockName("cutCake").disableStats().setRequiresSelfNotify();
+		
+		ModLoader.registerBlock(cutcake);
+		ModLoader.addName(cutcake, "\u5207");
+		
+		ModLoader.addRecipe(new ItemStack(cutcake), new Object[]
+				{
+					"ABA",
+					"ACA",
+					"ABA",
+					'A',Block.obsidian,
+					'B',Block.blockDiamond,
+					'C',Item.cake
+				});
+	}
+
+}
